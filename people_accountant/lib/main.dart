a350164fbadaf13c008d8c6754af3923c49b4055
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 11:13:41 -0300

    feat : turning widgets into stateful and created variable and algorithm to increment and decrement number of people

--- people_accountant/lib/main.dart
+++ people_accountant/lib/main.dart
@@ -1,57 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "People Accountant",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/new_game.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Pessoas: 0",
-                  style: TextStyle(
-                      color: Colors.purpleAccent, fontWeight: FontWeight.bold)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(
-                            fontSize: 40.0, color: Colors.purpleAccent),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(
-                            fontSize: 40.0, color: Colors.purpleAccent),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                "Pode Entrar!",
+  runApp(MaterialApp(title: "People Accountant", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoTexto = "Pode Entrar!";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+      if(_people < 0){
+        _infoTexto = "Mundo Invertido?";        
+      }
+      else if(_people<=10)
+      {
+        _infoTexto = "Pode Entrar!";        
+      }
+      else{
+        _infoTexto = "Lodado!"; 
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: $_people",
                 style: TextStyle(
-                    color: Colors.purpleAccent,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30),
-              )
-            ],
-          )
-        ],
-      )));
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "$_infoTexto",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
+            )
+          ],
+        )
+      ],
+    );
+  }
 }

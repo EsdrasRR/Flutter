282ea5606f2f5d8fc0caebb7f572aae64b009745
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 16:35:45 -0300

    feat : adding API call

--- currency_converter_using_api/lib/main.dart
+++ currency_converter_using_api/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main() {
+const request = "https://api.hgbrasil.com/finance?format=json&key=41df91f0";
+
+void main() async{
+
+  http.Response response = await http.get(request);
+  json.decode(response.body);
+
   runApp(MaterialApp(
-    
+    home: Container()
   ));
 }

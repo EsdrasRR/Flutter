f930fea3d3c55a9738e36cdeab22b314801a7ab1
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 10:50:31 -0300

    feat: added padding and background image

--- people_accountant/lib/main.dart
+++ people_accountant/lib/main.dart
@@ -3,35 +3,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "People Accountant",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text("Pessoa :0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
+          Image.asset(
+            "images/new_game.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Text("Pessoas: 0",
+                  style: TextStyle(
+                      color: Colors.purpleAccent, fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                            fontSize: 40.0, color: Colors.purpleAccent),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                            fontSize: 40.0, color: Colors.purpleAccent),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.purpleAccent,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30),
               )
             ],
-          ),
-          Text(
-            "Pode Entrar!",
-            style: TextStyle(
-                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
           )
         ],
       )));

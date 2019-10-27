66b20e0ec322734c24c0ff29bcb57b052e757416
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 13:48:26 -0300

    style : added scrollView and raisedButton

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -25,27 +25,49 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person, size: 120.0, color: Colors.blueGrey),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Weight (kg)",
-                labelStyle: TextStyle(color: Colors.blueGrey)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Height (cm)",
-                labelStyle: TextStyle(color: Colors.blueGrey)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person, size: 120.0, color: Colors.blueGrey),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Weight (kg)",
+                  labelStyle: TextStyle(color: Colors.blueGrey)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Height (cm)",
+                  labelStyle: TextStyle(color: Colors.blueGrey)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15, bottom: 15),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calculate",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueGrey, fontSize: 25.0),
+            )
+          ],
+        ),
       ),
     );
   }

b30f7585d9c5cd428097d004fb093c53490f5052
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 18:02:41 -0300

    refactor : creating a widget for the textfields

--- currency_converter_using_api/lib/main.dart
+++ currency_converter_using_api/lib/main.dart
@@ -19,6 +19,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dollarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dollar;
+  double euro;
+
+  void _realChanged(String text){
+
+  }
+  
+  void _dollarChanged(String text){
+
+  }
+  
+  void _euroChanged(String text){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,35 +49,73 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: FutureBuilder<Map>(
-          future: getData(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text("Loading data...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center),
+              );
+            default:
+              if (snapshot.hasError) {
                 return Center(
-                  child: Text("Loading data...",
+                  child: Text("Error loading data :(",
                       style: TextStyle(color: Colors.amber, fontSize: 25),
                       textAlign: TextAlign.center),
                 );
-              default:
-                if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Error loading data :(",
-                        style: TextStyle(color: Colors.amber, fontSize: 25),
-                        textAlign: TextAlign.center),
-                  );
-                } else {
-                  return Container(
-                    color: Colors.greenAccent,
-                  );
-                }
-            }
-          }),
+              } else {
+                dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                          size: 150, color: Colors.amber),
+                      buildTextField("Reais", "R\$", realController, _realChanged),
+                      Divider(),
+                      buildTextField("Dollars", "US\$", dollarController, _dollarChanged),
+                      Divider(),
+                      buildTextField("Euros", "€", euroController, _euroChanged)
+                    ],
+                  ),
+                );
+              }
+          }
+        },
+      ),
     );
   }
 }
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
+  print(json.decode(response.body));
   return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController controller, Function changeFunction) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+      prefixStyle: TextStyle(color: Colors.amber),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.amber),
+      ),
+    ),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+    onChanged: changeFunction,
+  );
 }

fc13df5a267635617193d23f04594d73adc9b748
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-27 11:54:13 -0300

    style: changed background image and text content

--- people_accountant/lib/main.dart
+++ people_accountant/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "People Accountant", home: Home()));
+  runApp(MaterialApp(title: "Otakus Accountant", home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -11,20 +11,17 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _people = 0;
-  String _infoTexto = "Pode Entrar!";
+  String _infoTexto = "You may come in!";
 
   void _changePeople(int delta) {
     setState(() {
       _people += delta;
-      if(_people < 0){
-        _infoTexto = "Mundo Invertido?";        
-      }
-      else if(_people<=10)
-      {
-        _infoTexto = "Pode Entrar!";        
-      }
-      else{
-        _infoTexto = "Lodado!"; 
+      if (_people < 0) {
+        _infoTexto = "SAD ='(";
+      } else if (_people < 10) {
+        _infoTexto = "You may come in!";
+      } else {
+        _infoTexto = "How many Otakus 0.0";
       }
     });
   }
@@ -34,54 +31,56 @@ class _HomeState extends State<Home> {
     return Stack(
       children: <Widget>[
         Image.asset(
-          "images/restaurant.jpg",
+          "images/happy_loli.jpg",
           fit: BoxFit.cover,
           height: 1000.0,
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Pessoas: $_people",
-                style: TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        Padding(
+            padding: EdgeInsets.only(top: 100.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                Text("Otakus: $_people",
+                    style: TextStyle(
+                        color: Colors.purpleAccent, fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          "+1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.purpleAccent),
+                        ),
+                        onPressed: () {
+                          _changePeople(1);
+                        },
+                      ),
                     ),
-                    onPressed: () {
-                      _changePeople(1);
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: FlatButton(
-                    child: Text(
-                      "-1",
-                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          "-1",
+                          style: TextStyle(fontSize: 40.0, color: Colors.purpleAccent),
+                        ),
+                        onPressed: () {
+                          _changePeople(-1);
+                        },
+                      ),
                     ),
-                    onPressed: () {
-                      _changePeople(-1);
-                    },
-                  ),
+                  ],
                 ),
+                Text(
+                  "$_infoTexto",
+                  style: TextStyle(
+                      color: Colors.purpleAccent,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 30),
+                )
               ],
-            ),
-            Text(
-              "$_infoTexto",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30),
-            )
-          ],
-        )
+            ))
       ],
     );
   }

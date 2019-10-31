51c4e277aa6fb1c69fe2fe7962b967a2155006ea
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-31 20:56:02 -0300

    style : starting layout

--- gif_searcher/lib/main.dart
+++ gif_searcher/lib/main.dart
@@ -2,7 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:gif_searcher/ui/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage()
-  ));
+  runApp(MaterialApp(home: HomePage()));
 }
--- gif_searcher/lib/ui/home_page.dart
+++ gif_searcher/lib/ui/home_page.dart
@@ -8,37 +8,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int _offset;
 
-  Future<Map >_getGifs() async {
+  Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null) {
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&limit=100&rating=G');
+    if (_search == null) {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&limit=100&rating=G');
     } else {
-      response = await http.get ('https://api.giphy.com/v1/gifs/search?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&q=$_search&limit=100&offset=$_offset&rating=G&lang=en');
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&q=$_search&limit=100&offset=$_offset&rating=G&lang=en');
     }
 
     return json.decode(response.body);
-
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    _getGifs().then((map){
+    _getGifs().then((map) {
       print(map);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            'https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black, 
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(13),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Search here ",
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

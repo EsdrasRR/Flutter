0085e1024bb6ababda524b211e6abeb31f7813ff
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-10-31 21:57:53 -0300

    feat : load more gifs

--- gif_searcher/lib/ui/home_page.dart
+++ gif_searcher/lib/ui/home_page.dart
@@ -9,8 +9,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _search;
-
-  int _offset;
+  int _offset = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
@@ -20,7 +19,7 @@ class _HomePageState extends State<HomePage> {
           'https://api.giphy.com/v1/gifs/trending?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&limit=100&rating=G');
     } else {
       response = await http.get(
-          'https://api.giphy.com/v1/gifs/search?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&q=$_search&limit=100&offset=$_offset&rating=G&lang=en');
+          'https://api.giphy.com/v1/gifs/search?api_key=4mst2PC1kFh4qnJN1vknNUcE4eqrarWw&q=$_search&limit=19&offset=$_offset&rating=G&lang=en');
     }
 
     return json.decode(response.body);
@@ -61,6 +60,11 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, fontSize: 18),
               textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                });
+              },
             ),
           ),
           Expanded(
@@ -94,22 +98,50 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
       padding: EdgeInsets.all(10),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
-          crossAxisSpacing: 10, 
-          mainAxisSpacing: 10 
-          ),
-        itemCount: snapshot.data['data'].length,
-        itemBuilder: (context, index) {
+          crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+      itemCount: _getCount(snapshot.data["data"]),
+      itemBuilder: (context, index) {
+        if (_search == null || index < snapshot.data['data'].length) {
           return GestureDetector(
-            child: Image.network(snapshot.data['data'][index]['images']['fixed_height']['url'],
-            height: 300,
-            fit: BoxFit.cover),
+            child: Image.network(
+                snapshot.data['data'][index]['images']['fixed_height']['url'],
+                height: 300,
+                fit: BoxFit.cover),
           );
-        },
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 70),
+                  Text(
+                    "Load more...",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  )
+                ],
+              ),
+              onTap: (){
+                setState(() {
+                 _offset += 19; 
+                });
+              },
+            ),
+          );
+        }
+      },
     );
   }
 }

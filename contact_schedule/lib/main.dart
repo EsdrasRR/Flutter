8d5385d38da61ca63fc52ee9f8dbe0488049eb4d
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-11-02 14:03:08 -0300

    feat : created contact page

--- contact_schedule/lib/main.dart
+++ contact_schedule/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:contact_schedule/helpers/contact_helper.dart';
+import 'package:contact_schedule/ui/contact_page.dart';
 import 'package:contact_schedule/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
   ));
 }

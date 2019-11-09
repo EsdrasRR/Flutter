c12ce03288bbb5571b2a51e25094140dcc2bde2c
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-11-09 14:54:13 -0300

    feat : finish initial layout

--- alcohol_or_gasoline/lib/main.dart
+++ alcohol_or_gasoline/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:alcohol_or_gasoline/widgets/input.widget.dart';
+import 'package:alcohol_or_gasoline/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Alcohol or Gasoline',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
     );
@@ -17,10 +20,37 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasController = new MoneyMaskedTextController();
+  var _alcController = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          Input(label: "Gasoline", controller: _gasController),
+          Input(label: "Alcohol", controller: _alcController),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(60)),
+            height: 50,
+            child: FlatButton(
+              child: Text(
+                "CALCULATE",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30,
+                    fontFamily: "Big Shoulders Display"),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ alcohol_or_gasoline/lib/widgets/logo.widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 60),
+        Image.asset("assets/images/aog-white.png", height: 100),
+        SizedBox(height: 20),
+        Text(
+          "Alcohol or Gasoline",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontFamily: "Big Shoulders Display",
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
+  }
+}
